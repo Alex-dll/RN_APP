0cc54fb80f09f64a3674cc496dc0019da391bdec
alex-dll <alexsandrojr.303@gmail.com>
2022-09-03 23:35:55 -0300

    feat: :tada: First commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const src: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.App}>
+      <Text style={styles.Title}>Wellcome to schina Academy</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  App: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+  Title: {
+    color: 'white',
+    fontSize: 30,
+  },
+});
+
+export default src;
