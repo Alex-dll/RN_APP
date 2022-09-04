5b59bbd457cc49f8569db789c7105f47710ad644
alex-dll <alexsandrojr.303@gmail.com>
2022-09-04 20:05:57 -0300

    feat: :heavy_plus_sign: Install Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Header from './components/Header';
 
 // import { Container } from './styles';
 
 const src: React.FC = () => {
   return (
     <SafeAreaView style={styles.App}>
-      <Text style={styles.Title}>Wellcome to schina Academy</Text>
+      <Header />
     </SafeAreaView>
   );
 };
@@ -15,11 +16,6 @@ const styles = StyleSheet.create({
   App: {
     flex: 1,
     backgroundColor: 'black',
-    justifyContent: 'center',
-  },
-  Title: {
-    color: 'white',
-    fontSize: 30,
   },
 });
 
