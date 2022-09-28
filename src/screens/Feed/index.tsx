4fc7fcfd87c0608e781d9fad9dfa20f0d942e050
alex-dll <alexsandrojr.303@gmail.com>
2022-09-27 22:41:38 -0300

    feat: :sparkles: Navigate neestede navigate

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -8,9 +8,16 @@ const Feed: React.FC = ({ navigation }) => {
     navigation.navigate('stories');
   };
 
+  const handlePressToPost = () => {
+    navigation.navigate('profile', {
+      screen: 'post',
+    });
+  };
+
   return (
     <View style={{ flex: 1, backgroundColor: '#ddd' }}>
       <Button title="Go to Stories" onPress={handlePress} />
+      <Button title="Go to Profile" onPress={handlePressToPost} />
     </View>
   );
 };
