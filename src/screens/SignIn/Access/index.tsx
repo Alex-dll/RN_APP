9aa7dcb7609490ded0959378133374fcc9cd7f01
Alex <alexsandrojr.303@gmail.com>
2023-01-24 00:10:13 -0300

    feat: :sparkles: Create Access Screen

--- src/screens/SignIn/Access/index.tsx
+++ src/screens/SignIn/Access/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useTheme } from 'styled-components/native';
 
-// import { Container } from './styles';
+import slashImage from '~/assets/images/splash/splashScreen.png';
+import Button from '~/components/Button';
+import Separator from '~/components/Separator';
+import Text from '~/components/Text';
+import useSignInNavigation from '~/hooks/useSignInNavigation';
 
-const Access: React.FC = () => {
-  return <View />;
+import {
+  Container,
+  ContainerRounded,
+  IconRoundedVaccine,
+  IconVaccine,
+  Column,
+} from './styles';
+
+const Access = () => {
+  const { bottom } = useSafeAreaInsets();
+  const { spacing } = useTheme();
+  const navigation = useSignInNavigation();
+
+  /**
+   * Callback
+   */
+  const handleNavigateToLogin = () => navigation.navigate('login');
+  const handleNavigateToSignUp = () => navigation.navigate('signUp');
+
+  return (
+    <Container source={slashImage}>
+      <StatusBar barStyle="light-content" />
+
+      <Column>
+        <IconRoundedVaccine>
+          <IconVaccine icon="vaccinePlus" size={80} />
+        </IconRoundedVaccine>
+        <Separator height={spacing.md} />
+        <Text typography="h2" color="background">
+          Vacinadx
+        </Text>
+      </Column>
+
+      <ContainerRounded>
+        <Separator height={spacing.xl} />
+        <Text typography="h4">Bem vindx</Text>
+        <Separator height={spacing.sm} />
+        <Text typography="subtitle1">Acesse ao app</Text>
+        <Separator height={spacing.xxl} />
+        <Button onPress={handleNavigateToLogin}>Login</Button>
+        <Separator height={spacing.lg} />
+        <Button mode="outline" onPress={handleNavigateToSignUp}>
+          Cadastro
+        </Button>
+        <Separator height={bottom + spacing.xxxl} />
+      </ContainerRounded>
+    </Container>
+  );
 };
 
 export default Access;
